598f1b00b39baea222795beac1646070f23487a2
Rubin <rubinradhakrishnan@gmail.com>
2020-08-28 18:18:07 +0530

    Add a table displaying countries and their corresponding no. of cases

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -9,11 +9,14 @@ import {
 import "./App.css";
 import InfoBox from "./InfoBox";
 import Map from "./Map";
+import Table from "./Table";
+import { sortData } from "./util";
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState("worldwide");
   const [countryInfo, setcountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
 
   useEffect(() => {
     fetch("https://disease.sh/v3/covid-19/all")
@@ -33,6 +36,8 @@ function App() {
             value: country.countryInfo.iso2,
           }));
 
+          const sortedData = sortData(data);
+          setTableData(sortedData);
           setCountries(countries);
         });
     };
@@ -103,7 +108,7 @@ function App() {
       <Card className="app__right">
         <CardContent>
           <h3>Live Cases by country</h3>
-          {/* Table */}
+          <Table countries={tableData} />
           <h3>Worldwide new Cases</h3>
           {/* Graph */}
         </CardContent>
